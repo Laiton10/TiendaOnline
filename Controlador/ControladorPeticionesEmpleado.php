59e0d18106110c4f5b9e9d79fb7f6ade3cb6d28f
Laiton10 <jesuscamposlaiton@gmail.com>
2024-11-04 12:07:36 +0100

    Borrado

--- Controlador/ControladorPeticionesEmpleado.php
+++ Controlador/ControladorPeticionesEmpleado.php
@@ -6,12 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <header><h1>SOY JESUS</h1></header>
-        <p>Me llamo jesus y soy andaluz</p>
-<p>ROldan es super gay</p>
-<p>Juan tambien es bastante homosexual</p>
-<p>hola</p>
-<p>hola2</p>
+<header>Peticiones</header>
 </body>
 </html>
 
