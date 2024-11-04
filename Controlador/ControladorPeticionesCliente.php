814e07481de0f8e54cc3e2692d31ae46c9c59acb
Laiton10 <jesuscamposlaiton@gmail.com>
2024-11-04 18:18:21 +0100

    Registro con validaciones + css

--- Controlador/ControladorPeticionesCliente.php
+++ Controlador/ControladorPeticionesCliente.php
@@ -6,38 +6,52 @@ $clienteDAO = new DAOCliente();
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_POST["id"]) &&  isset($_POST["nombre"]) &&  isset($_POST["apellido"]) && isset($_POST["nickname"]) && isset($_POST["password"]) && isset($_POST["telefono"]) && isset($_POST["domicilio"])){
 
-        if(!empty($_POST["id"])){
-            $id = $_POST["id"];
-        }else{
+        if (empty($_POST["id"])) {
             $avisoID = "El campo id es obligatorio";
         }
+        else {
+            $id = $_POST["id"];
+        }
 
-        if(!empty($_POST["nombre"])){
-            $nombre = $_POST["nombre"];
-        }else{
+        if (empty($_POST["nombre"])) {
             $avisoNombre = "El campo nombre es obligatorio";
+        }elseif(!ctype_alnum($_POST["nombre"])){
+            $avisoNombre = "El campo nombre solo puede tener caracteres alfanumericos";
+        }else {
+            $nombre = $_POST["nombre"];
         }
 
-            $apellido = $_POST["apellido"]; //no debe ser obligatorio
+        $apellido = $_POST["apellido"];
 
-        if(!empty($_POST["nickname"])){
-            $nickname = $_POST["nickname"];
-        }else{
+        if (empty($_POST["nickname"])) {
             $avisoNickname = "El campo nickname es obligatorio";
-        }
-        if(!empty($_POST["password"])){
-            $password = $_POST["password"];
-        }else{
-            $avisoPassword = "El campo password es obligatorio";
+        } else {
+            $nickname = $_POST["nickname"];
         }
 
-        if(!empty($_POST["telefono"])){
-            $telefono = $_POST["telefono"];
-        }else{
+        if (empty($_POST["password"])) {
+            $avisoPassword = "El campo password es obligatorio";
+        } elseif(strlen($_POST["password"]) < 4 || strlen($_POST["password"]) > 10 ){
+            $avisoPassword = "La contrasena debe tener entre 4 y 10 caracteres";
+        }elseif (!preg_match("/[A-Z]/",$_POST["password"])){ // preg match sirve para realizar una busqueda en una cadena utilizando una expresion regular
+            $avisoPassword = "La contrasena debe contener al menos una letra mayuscula";
+        }elseif (!preg_match("/[a-z]/",$_POST["password"])){
+            $avisoPassword = "La contrasena debe contener al menos una letra minuscula";
+        }elseif (!preg_match("/[0-9]/", $_POST["password"])){
+            $avisoPassword = "La contrasena debe contener al menos un numero";
+        }
+        else {
+            $password = $_POST["password"];
+        }
+
+        if (empty($_POST["telefono"])) {
             $avisoTelefono = "El campo telefono es obligatorio";
+        } else {
+            $telefono = $_POST["telefono"];
         }
 
         $domicilio = $_POST["domicilio"];
+
 
         if (!empty($avisoID) || !empty($avisoNombre) || !empty($avisoNickname) || !empty($avisoPassword) || !empty($avisoTelefono)) {
             header("Location: ../Vista/registroCliente.php?avisoID=$avisoID&avisoNombre=$avisoNombre&avisoNickname=$avisoNickname&avisoPassword=$avisoPassword&avisoTelefono=$avisoTelefono");
--- Vista/registroCliente.php
+++ Vista/registroCliente.php
@@ -3,20 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Registro</title>
+    <link href="registro.css" rel="stylesheet">
 </head>
 <body>
     <header><h1>Registro del cliente</h1></header>
+    <div class="container">
     <form action="../Controlador/ControladorPeticionesCliente.php" method="post">
-        <label for="id">ID: <input type="number" name="id" id="id"></label><span style="color: red"><?php echo $_REQUEST["avisoID"]?></span><br><br>
-        <label for="nombre">Nombre: <input type="text" name="nombre" id="nombre"></label><?php echo $_REQUEST["avisoNombre"]?><br><br>
-        <label for="apellido">Apellido: <input type="text" name="apellido" id="apellido"></label><br><br>
-        <label for="nickname">Nickname: <input type="text" name="nickname" id="nickname"></label><?php echo $_REQUEST["avisoNickname"]?><br><br>
-        <label for="password">Password: <input type="password" name="password" id="password"></label><?php echo $_REQUEST["avisoPassword"]?><br><br>
-        <label for="telefono">Telefono: <input type="number" name="telefono" id="telefono"></label><?php echo $_REQUEST["avisoTelefono"]?><br><br>
-        <label for="domicilio">Domicilio: <input type="text" name="domicilio" id="domicilio"></label><br><br>
+            <label for="id"><input type="number" name="id" id="id" placeholder="ID"></label><br>
+            <span style="color: red"><?php echo $_REQUEST["avisoID"] ?></span><br><br>
 
-        <button type="submit">Enviar datos</button>
-        <button type="reset">Borrar datos</button>
+            <label for="nombre"><input type="text" name="nombre" id="nombre" placeholder="Nombre"></label><br>
+            <span style="color: red"><?php echo $_REQUEST["avisoNombre"] ?></span><br><br>
+
+            <label for="apellido"><input type="text" name="apellido" id="apellido" placeholder="Apellido"></label>
+            <br><br>
+
+            <label for="nickname"><input type="text" name="nickname" id="nickname" placeholder="Nickname"></label><br>
+            <span style="color: red"><?php echo $_REQUEST["avisoNickname"] ?></span><br><br>
+
+            <label for="password"><input type="password" name="password" id="password" placeholder="Password"></label><br>
+            <span style="color: red"><?php echo $_REQUEST["avisoPassword"] ?></span><br><br>
+
+            <label for="telefono"><input type="number" name="telefono" id="telefono" placeholder="Telefono"></label><br>
+            <span style="color: red"><?php echo $_REQUEST["avisoTelefono"] ?></span><br><br>
+
+            <label for="domicilio"><input type="text" name="domicilio" id="domicilio" placeholder="Domicilio"></label><br><br>
+
+            <button type="submit">Enviar datos</button><br><br>
+            <button type="reset">Borrar datos</button>
     </form>
+    </div>
 </body>
 </html>
