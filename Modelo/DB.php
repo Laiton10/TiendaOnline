63845546582ed07170eed067f0b81ce44c7e25b4
roldaan04 <javirol2005@gmail.com>
2024-11-04 13:20:48 +0100

    DB

---
+++ Modelo/DB.php
@@ -0,0 +1,23 @@
+<?php
+
+class DB{
+ private static $host= "localhost";
+ private static $usuario= "root";
+ private static $password= "";
+ private static $dbName= "mi_tienda";
+
+ private static $conn= null;
+ private static function getConnection(){
+     if(self::$conn === null){
+         try{
+             self::$conn= new PDO("mysql:host=" . self::$host . ";dbname=" . self::$dbName, self::$usuario, self::$password);
+             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         }catch(PDOException $e){
+             echo "Error: " . $e->getMessage();
+         }
+     }
+     return self::$conn;
+ }
+}
+
+?>
